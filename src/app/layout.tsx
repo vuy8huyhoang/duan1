cc277d1b4716d0f44a6228a4541f673cdbab4188
vuy8huyhoang <vuy8huyhoang@gmail.com>
2024-10-11 19:41:01 +0700

    ssss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,19 @@
 import Sidebar from './component/sidebar';
 
 import "./globals.css";
-import axios from '@/lib/axios';
+// import axios from '@/lib/axios';
 
 export default function Layout({ children }: any) {
 
-  axios.get("/posts").then((r: any) => {
-    console.log(r);
-  })
+  // axios.get("/posts").then((r: any) => {
+  //   console.log(r);
+  // })
 
   return (
     <html lang="en">
       <head>
         <title>Groove</title>
+        
         <meta name="description" content="Soundy website description" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="../favicon.ico" /> */}
@@ -21,10 +22,9 @@ export default function Layout({ children }: any) {
       <body
       //  onClick={onClickBodyHandle}
       >
-        <div className="home-page">
-            <Sidebar />
-           
-             
+        <div className="container">
+          <Sidebar />
+          {/* Nội dung chính */}
         </div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   console.log(111);
 
   return (
-    <div className="home-page">
+    <div >
       
     </div>
   );
