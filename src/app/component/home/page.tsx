5aeb8870926e9484533014ac4eb1b548190fa9aa
vuy8huyhoang <vuy8huyhoang@gmail.com>
2024-10-16 19:10:53 +0700

    ccc

--- src/app/component/home/page.tsx
+++ src/app/component/home/page.tsx
@@ -6,6 +6,9 @@ import axios from 'axios';
 import ListMusic from '../listmusic';
 import ListAlbum from '../listalbum';
 import ListMusicTop from '../listmusictop';
+import ListType from '../listtype';
+import AlbumHot from '../albumhot';
+import MusicPartner from '../musicpartner';
 
 
 interface Album {
@@ -13,6 +16,7 @@ interface Album {
     name: string;
     url_cover: string;
 }
+
 
 export default function Home() {
     const [albumData, setAlbumData] = useState<Album[]>([]);
@@ -47,7 +51,10 @@ export default function Home() {
         </div>
         <ListMusic />
         <ListAlbum />
-        <ListMusicTop/>
+        <ListMusicTop />
+        <ListType />
+        <AlbumHot />
+        <MusicPartner/>
     </>
     );
 }
