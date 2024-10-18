0746a55c1bea6c7fd6c13ec4fa7be365af3950a4
food29912 <125857457+food29912@users.noreply.github.com>
2024-10-18 14:58:08 +0700

    sửa lại css header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,6 @@ export default function Layout({ children }: any) {
     <html lang="en">
       <head>
         <title>Groove</title>
-
         <meta name="description" content="Soundy website description" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="../favicon.ico" /> */}
