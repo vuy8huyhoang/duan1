166090fd09ae782a0e7539b90bd5c15881578fa0
chuvantruong <truongchu431@gmail.com>
2024-10-22 18:48:44 +0700

    ct

--- src/app/groovechart/page.tsx
+++ src/app/groovechart/page.tsx
@@ -61,7 +61,7 @@ export default function GrooveChartPage() {
                     </div>
                 ))}
             </div>
-            <Bxh />  
+            {/* <Bxh />   */}
             {/* <div className={styles.musicList}>
                 {musicData.map((music,index) => (
                     <div key={music.id_music} className={styles.musicCard}> 
@@ -69,6 +69,158 @@ export default function GrooveChartPage() {
                     </div>
                 ))}
             </div> */}
+    <div className={styles.chartInAreaMusicTotal}>
+  <div className={styles.chartInAreaMusicHeader}>
+    <span className={styles.chartInAreaMusicText}>Bảng Xếp Hạng Tuần</span>
+  </div>
+
+  <div className={styles.chartInAreaMusicList}>
+    {/* Bảng xếp hạng Việt Nam */}
+    <div className={styles.itemChartArea}>
+      <div className={styles.itemChartAreaMain}>
+        <div className={styles.chartHeader}>
+          <span className={styles.chartHeaderText}>Việt Nam</span>
+          <img
+            src="./assets/img/icon/music-chart.png"
+            alt=""
+            className={styles.chartHeaderImg}
+          />
+        </div>
+        <div className={styles.chartSongItem}>
+          <div className={styles.infoItemChart}>
+            <div className={styles.media}>
+              <div className={styles.mediaLeft}>
+                <div className={styles.songPrefix}>
+                  <span className={styles.number}>1</span>
+                  <div className={styles.sort}>
+                    <i className="far fa-window-minimize"></i>
+                  </div>
+                </div>
+                <div className={styles.songThumb}>
+                  <img
+                    src="./assets/img/picture_song/amee_pic.jpg"
+                    alt=""
+                    className={styles.songThumbChart}
+                  />
+                </div>
+                <div className={styles.cardInfo}>
+                  <div className={styles.nameSong}>Sao anh chưa về</div>
+                  <h3 className={styles.nameSinger}>Amee</h3>
+                </div>
+              </div>
+              <div className={styles.mediaRight}>
+                <div className={styles.timeSongItem}>
+                  <span className={styles.timeSong}>03:34</span>
+                </div>
+                <div className={styles.actionSongItem}></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.buttonWatch100}>
+          <button className={styles.watch100Btn}>Xem tất cả</button>
+        </div>
+      </div>
+    </div>
+
+    {/* Bảng xếp hạng Âu Mỹ */}
+    <div className={styles.itemChartArea}>
+      <div className={styles.itemChartAreaMain}>
+        <div className={styles.chartHeader}>
+          <span className={styles.chartHeaderText}>Âu Mỹ</span>
+          <img
+            src="./assets/img/icon/music-chart.png"
+            alt=""
+            className={styles.chartHeaderImg}
+          />
+        </div>
+        <div className={styles.chartSongItem}>
+          <div className={styles.infoItemChart}>
+            <div className={styles.media}>
+              <div className={styles.mediaLeft}>
+                <div className={styles.songPrefix}>
+                  <span className={styles.number}>1</span>
+                  <div className={styles.sort}>
+                    <i className="far fa-window-minimize"></i>
+                  </div>
+                </div>
+                <div className={styles.songThumb}>
+                  <img
+                    src="./assets/img/chart/baby.png"
+                    alt=""
+                    className={styles.songThumbChart}
+                  />
+                </div>
+                <div className={styles.cardInfo}>
+                  <div className={styles.nameSong}>INDUSTRY BABY</div>
+                  <h3 className={styles.nameSinger}>Lil Nas X, Jack Harlow</h3>
+                </div>
+              </div>
+              <div className={styles.mediaRight}>
+                <div className={styles.timeSongItem}>
+                  <span className={styles.timeSong}>03:34</span>
+                </div>
+                <div className={styles.actionSongItem}></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.buttonWatch100}>
+          <button className={styles.watch100Btn}>Xem tất cả</button>
+        </div>
+      </div>
+    </div>
+
+    {/* Bảng xếp hạng Hàn Quốc */}
+    <div className={styles.itemChartArea}>
+      <div className={styles.itemChartAreaMain}>
+        <div className={styles.chartHeader}>
+          <span className={styles.chartHeaderText}>Hàn Quốc</span>
+          <img
+            src="./assets/img/icon/music-chart.png"
+            alt=""
+            className={styles.chartHeaderImg}
+          />
+        </div>
+        <div className={styles.chartSongItem}>
+          <div className={styles.infoItemChart}>
+            <div className={styles.media}>
+              <div className={styles.mediaLeft}>
+                <div className={styles.songPrefix}>
+                  <span className={styles.number}>1</span>
+                  <div className={styles.sort}>
+                    <i className="far fa-window-minimize"></i>
+                  </div>
+                </div>
+                <div className={styles.songThumb}>
+                  <img
+                    src="./assets/img/chart/blue.jpg"
+                    alt=""
+                    className={styles.songThumbChart}
+                  />
+                </div>
+                <div className={styles.cardInfo}>
+                  <div className={styles.nameSong}>BLUE</div>
+                  <h3 className={styles.nameSinger}>Big Bang</h3>
+                </div>
+              </div>
+              <div className={styles.mediaRight}>
+                <div className={styles.timeSongItem}>
+                  <span className={styles.timeSong}>03:34</span>
+                </div>
+                <div className={styles.actionSongItem}></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.buttonWatch100}>
+          <button className={styles.watch100Btn}>Xem tất cả</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
             
         </div>
         
