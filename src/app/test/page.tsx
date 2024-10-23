c49b3b6a412ab6569a30101a216e50a77c10badb
panh9151 <panh9151@gmail.com>
2024-10-24 02:01:00 +0700

    change api

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,15 +2,7 @@
 import axios from "@/lib/axios";
 
 const Test = () => {
-  //   axios.get("music").then((data) => console.log(data));
-  fetch("https://api-groove.vercel.app/music", {
-    headers: {
-      "Content-Type": "application/json",
-      //   Authorization: "Bearer your-token-here",
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  axios.get("music").then((data) => console.log(data));
 
   return <>Test</>;
 };
