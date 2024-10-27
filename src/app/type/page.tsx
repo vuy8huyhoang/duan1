e6ff8c3b8625890948566ddc9e1bd46541a19746
food29912 <125857457+food29912@users.noreply.github.com>
2024-10-27 16:32:57 +0700

    ok

--- src/app/type/page.tsx
+++ src/app/type/page.tsx
@@ -3,11 +3,18 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Link from 'next/link';
 import style from './type.module.scss';
+import ListMusicTop from '../component/listmusictop';
+
+interface Type {
+  id_type: string;
+  name: string;
+}
 
 const TypePage = () => {
-  const [typeList, setTypeList] = useState<any[]>([]);
+  const [typeList, setTypeList] = useState<Type[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [showAll, setShowAll] = useState<boolean>(false); 
 
   const fetchTypeList = async () => {
     try {
@@ -26,6 +33,10 @@ const TypePage = () => {
     fetchTypeList();
   }, []); 
 
+  const handleShowAll = () => {
+    setShowAll(true); 
+  };
+
   if (loading) {
     return <div className={style.loading}>Đang tải dữ liệu...</div>;
   }
@@ -35,22 +46,31 @@ const TypePage = () => {
   }
 
   return (
-    <div className={style.container}>
-      <h1 className={style.title}>Danh Sách Thể Loại</h1>
-      <ul className={style.typeList}>
-        {Array.isArray(typeList) && typeList.length > 0 ? (
-          typeList.map((type: any) => (
-            <li key={type.id_type} className={style.typeItem}>
-              <Link href={`/type/${type.id_type}`} className={style.typeLink}>
-                {type.name}
-              </Link>
-            </li>
-          ))
-        ) : (
-          <li className={style.typeItem}>Không có thể loại nào.</li>
+    <>
+      <div className={style.container}>
+        <h1 className={style.title}>Danh Sách Thể Loại</h1>
+        <ul className={style.typeList}>
+          {Array.isArray(typeList) && typeList.length > 0 ? (
+            typeList.slice(0, showAll ? typeList.length : 5).map((type) => (
+              <li key={type.id_type} className={style.typeItem}>
+                <Link href={`/type/${type.id_type}`} className={style.typeLink}>
+                  {type.name}
+                </Link>
+              </li>
+            ))
+          ) : (
+            <li className={style.typeItem}>Không có thể loại nào.</li>
+          )}
+        </ul>
+        {!showAll && (
+          <button onClick={handleShowAll} className={style.showAllButton}>
+            Tất cả
+          </button>
         )}
-      </ul>
-    </div>
+      </div>
+      <ListMusicTop />
+      
+    </>
   );
 };
 
