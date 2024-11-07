57ae0ed237ea36c836790bd48061af7ceda00c0c
chuvantruong <truongchu431@gmail.com>
2024-11-08 03:18:53 +0700

    profile update

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -22,9 +22,9 @@ export default function Profile() {
     const [loading, setLoading] = useState(true);
     const [editingField, setEditingField] = useState<string | null>(null);
     const [editValue, setEditValue] = useState<string>('');
-    const [src, setSrc]= useState<string>('');
     const [file, setFile] = useState(null); // Lưu trữ file người dùng chọn
     const [message, setMessage] = useState(""); // Thông báo kết quả
+    const [imagePreview, setImagePreview] = useState<string | null>(null); // Add state for image preview
     useEffect(() => {
         axios.get("profile")
             .then((response: any) => {
@@ -56,44 +56,68 @@ export default function Profile() {
         
         return `${day}-${month}-${year}`;
     }
+    
 
 
-     // Xử lý sự kiện khi người dùng chọn file
+// Xử lý sự kiện khi người dùng chọn file
   const handleFileChange = (event:any) => {
-    setFile(event.target.files[0]);
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+    
+    if (selectedFile) {
+        const fileURL = URL.createObjectURL(selectedFile); // Create a URL for the selected file
+        setImagePreview(fileURL); // Set the image preview state
+    }
   };
 // Xử lý sự kiện submit form
 const handleSubmit = async (event:any) => {
     event.preventDefault();
     
     if (!file) {
-      setMessage("Vui lòng chọn một file.");
-      return;
+        alert("Chưa có ảnh");
+        return;
+    }
+
+    // Validate file type and size (example: only allow images under 2MB)
+    if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type) || file.size > 2 * 1024 * 1024) {
+        alert("Vui lòng chọn một tệp hình ảnh hợp lệ dưới 2MB.");
+        return;
     }
 
     const formData = new FormData();
     formData.append("file", file);
 
     try {
-      const response: any = await axios.post("/upload-image", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-      
+        const response: any = await axios.post("/upload-image", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
+        alert("Tải ảnh lên thành công");
+        console.log(response);
 
-      const profileResponse: any = await axios.patch("/update-infor", {
-             url_avatar: response.result.url
-      })
-      
-    } catch (error:any) {
-      setMessage("Tải lên thất bại: " + error);
+        const profileResponse: any = await axios.patch("/update-infor", {
+            url_avatar: response.result.url
+        });
+        
+    } catch (error: any) {
+        alert("Tải ảnh lên thất bại: " + error);
     }
-  };
+};
     
     
     const handleSaveClick = () => {
         if (profileData && editingField) {
+            // Validate updated profile data
+            if (editingField === 'phone' && !/^\d+$/.test(editValue)) {
+                alert("Số điện thoại không hợp lệ.");
+                return;
+            }
+            if (editingField === 'email' && !/\S+@\S+\.\S+/.test(editValue)) {
+                alert("Email không hợp lệ.");
+                return;
+            }
+
             const updatedProfile = { ...profileData, [editingField]: editValue };
             setProfileData(updatedProfile);
             setEditingField(null);
@@ -117,47 +141,139 @@ const handleSubmit = async (event:any) => {
                 <h1>Xin chào, <span>{profileData?.fullname || 'Guest'}</span>!</h1>
             </div>
             <div className={style.profileDetails}>
-                <div className={style.profileimg}>
+                {/* <div className={style.profileimg}>
                     <img src="/Setting.svg" alt="" />
                     <img 
                         src={profileData?.url_avatar} alt=""
                     />
-                </div>
-                <div>
-                        <h2>Upload Image</h2>
-                        <form onSubmit={handleSubmit}>
-                            <input type="file" onChange={handleFileChange} />
-                            <button type="submit">Upload</button>
-                        </form>
-                        {message && <p>{message}</p>}
+                </div> */}
+                <div className={style.profileimg}>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="file-upload">
+                            <img 
+                            src={imagePreview || profileData?.url_avatar} // Use imagePreview if available
+                            alt="Profile Avatar" 
+                            style={{ cursor: 'pointer' }} // Change cursor to pointer for better UX
+                        />
+                        </label>
+                        <input 
+                            type="file" 
+                            id="file-upload" 
+                            onChange={handleFileChange} 
+                            style={{ display: 'none' }}
+                        />
+                        <div>
+                        <button type="submit">Upload</button>
+                        {message && <p className={style.message}>{message}</p>}
+                        </div>
+                        
+                    </form>
+                   
                 </div>
                 {/* <input type="file" multiple /> */}
                 <div className={style.info}>
                     <div className={style.infoItem}>
                         <p><strong>Email:</strong> {profileData?.email}</p>
                     </div>
-                    
-                    {['phone', 'birthday', 'gender', 'country'].map((field) => (
-                        <div className={style.infoItem} key={field}>
-                            <p>
-                                <strong>{field.charAt(0).toUpperCase() + field.slice(1)}:</strong> 
-                                {editingField === field ? (
-                                    <input 
-                                        type="text" 
-                                        value={field ==='birthday'?formatDate(editValue):editValue} 
-                                        onChange={(e) => setEditValue(e.target.value)} 
-                                    />
-                                ) : (
-                                    profileData?.[field as keyof Profile]
-                                )}
-                            </p>
-                            {editingField === field ? (
-                                <button className={style.editButton} onClick={handleSaveClick}>Lưu</button>
+
+                    {/* Phone Input */}
+                    <div className={style.infoItem}>
+                        <p>
+                            <strong>Phone:</strong> 
+                            {editingField === 'phone' ? (
+                                <input 
+                                    type="text" 
+                                    value={editValue} 
+                                    onChange={(e) => setEditValue(e.target.value)} 
+                                />
                             ) : (
-                                <button className={style.editButton} onClick={() => handleEditClick(field, profileData?.[field as keyof Profile] || '')}>Sửa thông tin</button>
+                                profileData?.phone
                             )}
-                        </div>
-                    ))}
+                        </p>
+                        {editingField === 'phone' ? (
+                            <button className={style.editButton} onClick={handleSaveClick}>Lưu</button>
+                        ) : (
+                            <button className={style.editButton} onClick={() => handleEditClick('phone', profileData?.phone || '')}>Sửa thông tin</button>
+                        )}
+                    </div>
+
+                    {/* Birthday Input */}
+                    <div className={style.infoItem}>
+                        <p>
+                            <strong>Birthday:</strong> 
+                            {editingField === 'birthday' ? (
+                                <input 
+                                    type="date" 
+                                    value={editValue} 
+                                    onChange={(e) => setEditValue(e.target.value)} 
+                                />
+                            ) : (
+                                formatDate(profileData?.birthday)
+                            )}
+                        </p>
+                        {editingField === 'birthday' ? (
+                            <button className={style.editButton} onClick={handleSaveClick}>Lưu</button>
+                        ) : (
+                            <button className={style.editButton} onClick={() => handleEditClick('birthday', profileData?.birthday || '')}>Sửa thông tin</button>
+                        )}
+                    </div>
+
+                    {/* Gender Input */}
+                    <div className={style.infoItem}>
+                        <p>
+                            <strong>Gender:</strong> 
+                            {editingField === 'gender' ? (
+                                <div>
+                                    <label>
+                                        <input 
+                                            type="radio" 
+                                            value="male" 
+                                            checked={editValue === 'male'} 
+                                            onChange={() => setEditValue('male')} 
+                                        />
+                                        Male
+                                    </label>
+                                    <label>
+                                        <input 
+                                            type="radio" 
+                                            value="female" 
+                                            checked={editValue === 'female'} 
+                                            onChange={() => setEditValue('female')} 
+                                        />
+                                        Female
+                                    </label>
+                                </div>
+                            ) : (
+                                profileData?.gender
+                            )}
+                        </p>
+                        {editingField === 'gender' ? (
+                            <button className={style.editButton} onClick={handleSaveClick}>Lưu</button>
+                        ) : (
+                            <button className={style.editButton} onClick={() => handleEditClick('gender', profileData?.gender || '')}>Sửa thông tin</button>
+                        )}
+                    </div>
+
+                    {/* Country Input */}
+                    <div className={style.infoItem}>
+                        <p>
+                            <strong>Country:</strong> 
+                            {editingField === 'country' ? (
+                                <input 
+                                    type="text" 
+                                    value={editValue} 
+                                    onChange={(e) => setEditValue(e.target.value)} 
+                                />
+                            ) : (
+                                profileData?.country
+                            )}
+                        </p>
+                        {editingField === 'country' ? (
+                            <button className={style.editButton} onClick={handleSaveClick}>Lưu</button>
+                        ) : (
+                            <button className={style.editButton} onClick={() => handleEditClick('country', profileData?.country || '')}>Sửa thông tin</button>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
