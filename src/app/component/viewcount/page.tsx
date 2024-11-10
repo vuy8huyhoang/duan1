ff665027029d67918304acb244620731a20a2492
tuankiet1412 <kietdeptrai1912@gmail.com>
2024-11-10 15:54:55 +0700

    sup

--- src/app/component/viewcount/page.tsx
+++ src/app/component/viewcount/page.tsx
@@ -4,24 +4,24 @@
 import React, { useEffect, useState } from "react";
 import axios from "@/lib/axios";
 
-interface ViewCountProps {
-  musicId: string;
+interface Music {
+  id_music: string;
 }
 
-const ViewCount: React.FC<ViewCountProps> = ({ musicId }) => {
+const ViewCount: React.FC<Music> = ({ id_music }) => {
   const [viewCount, setViewCount] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchViewCount = async () => {
       try {
-        const response = await axios.get(`/music/view-count/${musicId}`);
+        const response = await axios.get(`/music/view-count/${id_music}`);
         setViewCount(response.data.view_count);
       } catch (error) {
         console.error("Failed to fetch view count", error);
       }
     };
     fetchViewCount();
-  }, [musicId]);
+  }, [id_music]);
 
   return (
     <p>Lượt xem: {viewCount !== null ? viewCount : "Đang tải..."}</p>
